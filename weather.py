2f346a34aeb2ba74c6854343f60dd400b15ec8eb
Mark Sossoman <msossoman@gmail.com>
2015-04-04 19:18:33 -0400

    first commit countries

--- weather.py
+++ weather.py
@@ -1,13 +1,17 @@
-#This will be the module for the lecture 49 assignment using the weather API.
-
 import json
 
-jsonFile = open('countries.json', 'r')
-jsonOutput = json.loads(jsonFile)
+def countries():
+	jsonFile = open('countries.json', 'r')
+	lines = jsonFile.read()
+	
+	print lines[0][0]
 
-from requests import *
+if __name__ == "__main__":
+	countries()
+
+'''from requests import *
 
 query = get("http://api.openweathermap.org/data/2.5/weather?q=San+Francisco,us")
 
 query.json()
-query.text
+query.text'''
